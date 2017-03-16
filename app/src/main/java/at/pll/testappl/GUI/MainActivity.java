4842b63a6ee044f4752314b613ac4c1a7607e436
Pascal-AUT <laggerpas@gmail.com>
2017-03-16 17:32:28 +0100

    added GUI

---
+++ app/src/main/java/at/pll/testappl/GUI/MainActivity.java
@@ -0,0 +1,15 @@
+package at.pll.testappl.GUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import at.pll.testappl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/at/pll/testappl/GUI/QuizActivity.java
+++ app/src/main/java/at/pll/testappl/GUI/QuizActivity.java
@@ -1,13 +1,15 @@
-package at.pll.testappl;
+package at.pll.testappl.GUI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import at.pll.testappl.R;
+
+public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz);
     }
 }
